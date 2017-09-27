a79d1804dcbc9cbf34c3c797188ec41d5e25d7ef
m0sk1t <m0sk1t@bk.ru>
2017-09-27 17:27:42 +0300

    Command line args in init scripts

--- scripts/consumer/init_consumer.py
+++ scripts/consumer/init_consumer.py
@@ -1,7 +1,14 @@
+import sys
 from consumer import AConsumer
 
+#34.214.200.68
 def run():
-    a_consumer = AConsumer()
+    args = {'host': 'localhost', 'port': '9092'}
+    for arg in sys.argv:
+        entry = arg.split("=")
+        if len(entry) == 2:
+            args[str(entry[0])] = str(entry[1])
+    a_consumer = AConsumer(args['host'], args['port'])
     a_consumer.listen('test-topic')
 
 run()
--- scripts/producer/init_producer.py
+++ scripts/producer/init_producer.py
@@ -1,7 +1,13 @@
+import sys
 from producer import AProducer
 
 def run():
-    a_producer = AProducer()
+    args = {'host': 'localhost', 'port': '9092'}
+    for arg in sys.argv:
+        entry = arg.split("=")
+        if len(entry) == 2:
+            args[str(entry[0])] = str(entry[1])
+    a_producer = AProducer(args['host'], args['port'])
     while True:
         try:
             data = raw_input('enter your data: ')
