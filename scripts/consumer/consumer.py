9b7088b0fece06d42b17c63c047806f5c8f4dcfb
m0sk1t <m0sk1t@bk.ru>
2017-09-28 19:27:36 +0300

    Amazon SNS bindings

--- scripts/consumer/consumer.py
+++ scripts/consumer/consumer.py
@@ -1,5 +1,7 @@
 import json
 import time
+import boto3
+
 from kafka import KafkaConsumer
 
 
@@ -14,9 +16,16 @@ class AConsumer():
     def __connect(self, cb, topic):
         try:
             self.consumer = KafkaConsumer(bootstrap_servers=self.__server())
+            self.client = boto3.client(
+                "sns",
+                aws_access_key_id="AKIAI7ANTPLUGZATSHOQ",
+                aws_secret_access_key="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",
+                region_name="us-west-2"
+            )
             print "connected to host: ", self.__host, ", ", "port: ", self.__port
             cb(topic)
-        except:
+        except BaseException as e:
+            print e
             time.sleep(1)
             print "connecting..."
             self.__connect(cb, topic)
@@ -24,10 +33,22 @@ class AConsumer():
     def listen(self, topic):
         if hasattr(self, 'consumer'):
             self.consumer.subscribe(topic)
+            topic_arn = self.client.create_topic(Name=topic)['TopicArn']
             for msg in self.consumer:
                 try:
-                    print json.loads(msg.value)
-                except:
+                    message = json.loads(msg.value)
+                    print message
+                    self.client.subscribe(
+                        TopicArn=topic_arn,
+                        Protocol=message["proto"],
+                        Endpoint=message["contact"]
+                    )
+                    print self.client.publish(
+                        TopicArn=topic_arn,
+                        Message=message["message"]
+                    )
+                except BaseException as e:
+                    print e
                     print msg.value
         else:
             self.__connect(self.listen, topic)
--- scripts/producer/init_producer.py
+++ scripts/producer/init_producer.py
@@ -6,26 +6,26 @@ from producer import AProducer
 
 def run():
     mocks = [
+        # {
+        #     'proto': 'sms',
+        #     'contact': '+79997305889',
+        #     'message': 'test message from space to m0sk1t phone'
+        # },
         {
-            'type': 'phone',
-            'contact': '+79997305889',
-            'message': 'test message from space to m0sk1t phone'
-        },
-        {
-            'type': 'mail',
+            'proto': 'email',
             'contact': 'm0sk1t@bk.ru',
             'message': 'test message from space to m0sk1t mail'
         },
+        # {
+        #     'proto': 'sms',
+        #     'contact': '+79158334170',
+        #     'message': 'test message from space to artlitvinov phone'
+        # },
         {
-            'type': 'phone',
-            'contact': '+79158334170',
-            'message': 'test message from space to artlitvinov phone'
-        },
-        {
-            'type': 'mail',
+            'proto': 'email',
             'contact': 'work.litvinov.artem@gmail.com',
             'message': 'test message from space to artlitvinov mail'
-        }
+        },
     ]
     args = {'host': 'localhost', 'port': '9092'}
     for arg in sys.argv:
@@ -33,17 +33,13 @@ def run():
         if len(entry) == 2:
             args[str(entry[0])] = str(entry[1])
     a_producer = AProducer(args['host'], args['port'])
-'''
-     for message in mocks:
-        time.sleep(1)
-        a_producer.send('test-topic', message)
-'''
 
     while True:
-        time.sleep(1)
-        a_producer.send('test-topic', mocks[random.randint(0, 3)])
-
-
+        message = raw_input("Enter your message: ")
+        mock = mocks[random.randint(0, 3)]
+        mock["message"] = message
+        a_producer.send('test-topic', mock)
+        print "your message sent to", mock
 
 if __name__ == "__main__":
     run()
