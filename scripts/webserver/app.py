a1581689f60ca7f574f92ccb6946364d0b64b79c
m0sk1t <m0sk1t@bk.ru>
2017-10-02 15:11:05 +0300

    Clearing project

--- scripts/webserver/app.py
+++ scripts/webserver/app.py
@@ -1,10 +1,19 @@
 import time
-from flask import Flask, jsonify, request
 from cassandra.cluster import Cluster
+from flask import Flask, jsonify, redirect, request, render_template
 
 app = Flask(__name__)
 cluster = Cluster(['localhost'])
 session = cluster.connect('users')
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/sender')
+def sender():
+    return render_template('sender.html')
 
 @app.route('/user/<id>', methods=['GET'])
 def user(id):
@@ -14,16 +23,18 @@ def user(id):
         rows = session.execute("SELECT * FROM users")
         for row in rows:
             users.append({
+                'uid': row.uid,
+                'name': row.name,
                 'type': row.type,
-                'user_id': row.user_id,
                 'contact': row.contact,
             })
         return jsonify(users)
     else:
-        user = session.execute("SELECT * FROM users WHERE user_id='%s'" % (id))[0]
+        user = session.execute("SELECT * FROM users WHERE uid='%s'" % (id))[0]
         return jsonify({
+            'uid': user.uid,
+            'name': user.name,
             'type': user.type,
-            'user_id': user.user_id,
             'contact': user.contact,
         })
 
@@ -31,10 +42,9 @@ def user(id):
 def create_user():
     req = request.form
     session.execute('USE users')
-    user_id = str(int(round(time.time() * 1000)))
-#    print req["contact"]
-    print session.execute("INSERT INTO users (user_id, type, contact) VALUES (%s, %s, %s)", (user_id, req["type"], req["contact"]))
-    return user_id
+    uid = str(int(round(time.time() * 1000)))
+    print session.execute("INSERT INTO users (uid, name, type, contact) VALUES (%s, %s, %s, %s)", (uid, req["name"],  req["type"], req["contact"]))
+    return redirect('/sender')
 
 if __name__ == '__main__':
     app.run(debug=True)
