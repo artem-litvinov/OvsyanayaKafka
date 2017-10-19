ddd131973fa99572a49a92753cbf0fa30f77b216
m0sk1t <m0sk1t@bk.ru>
2017-10-19 13:18:02 +0300

    users route works

--- aiowebserver/app.py
+++ aiowebserver/app.py
@@ -7,28 +7,12 @@ from aiohttp import web
 from routes import setup_routes
 
 if __name__ == '__main__':
-    WEBSERVER_PORT=8080
     logger = logging.getLogger(__name__)
-    
-    '''
-        try:
-            WEBSERVER_PORT = os.environ['WEBSERVER_PORT']
-        except KeyError as err:
-            logger.error("KeyError! Please set WEBSERVER_PORT environment variable")
-            raise
-
-        try:
-            KAFKA_HOST = os.environ['KAFKA_HOST']
-        except KeyError as err:
-            logger.error("KeyError! Please set KAFKA_HOST environment variable")
-            raise
-
-        try:
-            CASSANDRA_HOST = os.environ['CASSANDRA_HOST']
-        except KeyError as err:
-            logger.error("KeyError! Please set CASSANDRA_HOST environment variable")
-            raise
-    '''
+    try:
+        WEBSERVER_PORT = os.environ['WEBSERVER_PORT']
+    except KeyError as err:
+        logger.error("KeyError! Please set WEBSERVER_PORT environment variable! Using port 8080")
+        WEBSERVER_PORT=8080
 
     app = web.Application()
     setup_routes(app)
