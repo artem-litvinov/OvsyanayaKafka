2888f09817248b7736caa4a5f8af2c4356a52643
m0sk1t <m0sk1t@bk.ru>
2017-10-20 18:26:47 +0300

    All stuff together

--- consumer/init_consumer.py
+++ consumer/init_consumer.py
@@ -2,76 +2,72 @@ import os
 import time
 import logging
 
+import boto3
+import asyncio
 from consumer import Consumer
+from aio_cassandra import create_cassandra
+from aiokafka_consumer import create_consumer
 from kafka_message.ttypes import KafkaMessage
 
-import boto3
 from thrift.TSerialization import deserialize
-from cassandra.cluster import Cluster
+
+loop = asyncio.get_event_loop()
 
 if os.environ.has_key("DEBUG"):
     logging.basicConfig(level=logging.DEBUG)
 
 def send_msg(client, contact, text):
-    print client.publish(
-        TopicArn="test-topic",
+    print (client.publish(
+        TopicArn="test-topic", 
         Message=text
-        )
-
-def get_user_data(uid, session):
-    user_rows = session.execute("SELECT * FROM users WHERE uid='%s'" % (uid))
-
-    for user_row in user_rows:
-        return user_row
-
-def get_message_data(mid, session):
-    msg_rows = session.execute("SELECT * FROM messages WHERE mid='%s'" % (mid))
-
-    for msg_row in msg_rows:
-        return msg_row
+        ))
 
 def deserialize_msg(msg):
     kafka_message = KafkaMessage()
     deserialize(kafka_message, msg.value)
     return kafka_message
 
-def kafka_message_handler(session, sns_client, kafka_msg):
-    kafka_msg = deserialize_msg(kafka_msg)
-    logging.debug("Received message %s from Kafka", kafka_msg)
+async def get_message_data(mid, session):
+    msg_rows = await session.get_from_table_by_id("messages", str(mid))
+    if len(msg_rows) > 0:
+        return msg_rows[0]
+    return None
 
-    msg = get_message_data(kafka_msg.mid, session)
+async def get_user_data(uid, session):
+    user_rows = await session.get_from_table_by_id("users", str(uid))
+    if len(user_rows) > 0:
+        return user_rows[0]
+    return None
+
+async def kafka_message_handler(session, sns_client, kafka_msg):
+    kafka_msg = deserialize_msg(kafka_msg)
+    msg = await get_message_data(kafka_msg.id, session)
     if msg is None:
         logging.warning(
             "Couldn't find message details for message %s", kafka_msg.mid)
         return
-
-    user = get_user_data(msg.uid, session)
+    user = await get_user_data(msg.uid, session)
     if user is None:
         logging.warning(
             "Couldn't find user details for message %s", msg.uid)
         return
+    print("send_msg(sns_client, %s, %s)" % (user.contact, msg.text))
 
-    print "send_msg(sns_client, %s, %s)" % (user.contact, msg.text)
+async def main():
+    sns_client = boto3.client("sns", region_name='us-west-2')
+    cassandra = create_cassandra()
+    cassandra.connect(loop, keyspace='users')
 
+    consumer = create_consumer('test-topic', 'test')
+    kafka_stream = await consumer.consume()
+    try:
+        async for kafka_msg in kafka_stream:
+            print(kafka_msg)
+            await kafka_message_handler(cassandra, sns_client, kafka_msg)
+    finally:
+        await consumer.stop()
 
 if __name__ == "__main__":
-    try:
-        CASSANDRA_HOST = os.environ['CASSANDRA_HOST']
-    except KeyError as err:
-        print(err, "Please set CASSANDRA_HOST environment variable")
-        raise
-
-    try:
-        KAFKA_HOST = os.environ['KAFKA_HOST']
-    except KeyError as err:
-        print(err, "Please set KAFKA_HOST environment variable")
-        raise
-
-    cluster = Cluster([CASSANDRA_HOST])
-    session = cluster.connect('users')
-    consumer = Consumer(KAFKA_HOST, '9092')
-    sns_client = boto3.client("sns", region_name='us-west-2')
-
-    for kafka_msg in consumer.messages('test-topic'):
-        kafka_message_handler(session, sns_client, kafka_msg)
-
+    loop.run_until_complete(main())
+    loop.close()
+        
